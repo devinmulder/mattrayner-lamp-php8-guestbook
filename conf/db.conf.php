b1d05be3e2e8e1864bbfe387e9f5417f60ce3b50
amd <amd@devin.com>
2023-05-17 20:05:24 +0900

    DB 를 별도의 Docker Container(MariaDB) 로 설정함.

--- conf/db.conf.php
+++ conf/db.conf.php
@@ -1,6 +1,18 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'admin';
+/**
+ * built-in DB
+ */
+//$db_host = 'localhost';
+//$db_user = 'admin';
+//$db_pass = 'apmsetup';
+//$db_name = 'brain_php';
+
+/**
+ * MariaDB in Docker
+ */
+$db_host = '172.17.0.3';
+$db_user = 'root';
 $db_pass = 'apmsetup';
 $db_name = 'brain_php';
+
 ?>
